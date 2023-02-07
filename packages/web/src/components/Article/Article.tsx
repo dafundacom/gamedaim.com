10f799e71d6da4f50418ea1be5dc3c26f2fb5998
Karyana Yandi <karyana@yopun.com>
2023-02-07 14:43:03 +0700

    fix(web): error css runtime

--- packages/web/src/components/Article/Article.tsx
+++ packages/web/src/components/Article/Article.tsx
@@ -91,7 +91,7 @@ export const Article = (props: { post: any; posts: any; index: any }) => {
           </>
         )}
         <div className="flex">
-          <div className="h-fit mr-2 shadow-xs fixed top-[unset] bottom-0 left-0 z-40 mx-0 mb-0 mr-0 flex w-full flex-row items-center justify-center bg-white dark:bg-gray-700 lg:px-2 lg:!sticky lg:top-20 lg:bottom-[unset] lg:left-[unset] lg:!w-auto lg:bg-transparent lg:shadow-none lg:dark:bg-transparent">
+          <div className="h-fit mr-2 shadow-sm fixed top-[unset] bottom-0 left-0 z-40 mx-0 mb-0 mr-0 flex w-full flex-row items-center justify-center bg-white dark:bg-gray-700 lg:px-2 lg:!sticky lg:top-20 lg:bottom-[unset] lg:left-[unset] lg:!w-auto lg:bg-transparent lg:shadow-none lg:dark:bg-transparent">
             <ShareButtonArticle
               url={`https://${env.DOMAIN}/${primary.slug}/${post.slug}`}
               text={title}
