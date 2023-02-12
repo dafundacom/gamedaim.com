a6e120740d6edf33d8436a529bccede5f180cd94
ghizzkira <tiadiagis@gmail.com>
2023-02-12 16:16:35 +0700

    feat(web): add menu mobile dashboard

--- packages/web/src/layouts/Dashboard.tsx
+++ packages/web/src/layouts/Dashboard.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
-
+import NextLink from "next/link"
 import { SidebarDashboard } from "@/components/Navigation"
+import { Button } from "ui"
 
 interface DashboardLayoutProps extends React.HTMLAttributes<HTMLDivElement> {}
 
@@ -14,6 +15,26 @@ export const DashboardLayout: React.FunctionComponent<DashboardLayoutProps> = (
       <SidebarDashboard />
       <div className="max-lg:pl-[65px] lg:pl-[270px] pr-3 transition-all dashboard-mainmenu">
         {children}
+      </div>
+      <div className="flex items-center justify-around fixed bottom-0 inset-x-0 bg-white border-t z-[9999]">
+        <NextLink
+          className="h-12 text-center cursor-pointer text-gray-800 flex flex-col items-center justify-around pt-[10px] px-[10px] pb-[15px] basis-1/3"
+          href="/dashboard"
+        >
+          <Button>Dashboard</Button>
+        </NextLink>
+        <NextLink
+          className="h-12 text-center cursor-pointer text-gray-800 flex flex-col items-center justify-around pt-[10px] px-[10px] pb-[15px] basis-1/3"
+          href="/dashboard/articles"
+        >
+          <Button>Articles</Button>
+        </NextLink>
+        <NextLink
+          className="h-12 text-center cursor-pointer text-gray-800 flex flex-col items-center justify-around pt-[10px] px-[10px] pb-[15px] basis-1/3"
+          href="/dashboard"
+        >
+          <Button>More</Button>
+        </NextLink>
       </div>
     </div>
   )
